cdb39f75df6dfb9f46980f0e9c2a82e32e3411f6
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2021-11-29 03:02:18 +0100

    commit 2

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ const toggleButton = () => {
   }
 
   loginWithMetaMask();
+  if (window.userWalletAddress) {
+    loginButton.innerText = "Logout of MetaMask";
+  }
 };
 
 async function loginWithMetaMask() {
-  const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  let accounts;
+  if (window.ethereum.chainId !== "0x38") {
+    accounts = await ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: "0x38" }],
+    });
+  } else {
+    accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  }
+  windows.userWalletAddress = accounts[0];
 }
