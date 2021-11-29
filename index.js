13c52d25017e3dad5e7cbbc0d27f094f4ac94670
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2021-11-29 08:24:28 +0100

    updated metamask function to add new Network

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 // });
 
 const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("click", () => toggleButton());
+
 window.userWalletAddress = null;
+
 const toggleButton = () => {
   if (!window.ethereum) {
     alert("MetaMask is not installed!");
@@ -17,13 +20,41 @@ const toggleButton = () => {
 
 async function loginWithMetaMask() {
   let accounts;
+
   if (window.ethereum.chainId !== "0x38") {
-    accounts = await ethereum.request({
-      method: "wallet_switchEthereumChain",
-      params: [{ chainId: "0x38" }],
+    await ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          chainId: "0x38",
+          blockExplorerUrls: ["https://bscscan.com"],
+          chainName: "BINANCE SMART CHAIN",
+          iconUrls: "",
+          nativeCurrency: {
+            name: "BINANCE",
+            symbol: "BNB",
+            decimals: 18,
+          },
+          rpcUrls: ["https://bsc-dataseed.binance.org/"],
+        },
+      ],
     });
+    accounts = await ethereum.request({ method: "eth_requestAccounts" });
   } else {
     accounts = await ethereum.request({ method: "eth_requestAccounts" });
   }
-  windows.userWalletAddress = accounts[0];
+  window.userWalletAddress = accounts[0];
 }
+
+// {
+//   chainId: string;
+//   blockExplorerUrls?: string[];
+//   chainName?: string;
+//   iconUrls?: string[];
+//   nativeCurrency?: {
+//     name: string;
+//     symbol: string;
+//     decimals: number;
+//   };
+//   rpcUrls?: string[];
+// }
